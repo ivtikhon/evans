45640e1e35597b14d3785ed571d9eee0824d43f2
Igor Tikhonin <ivtikhon@gmail.com>
2017-11-29 11:45:51 -0500

    Working on instance definition

--- ec2.py
+++ ec2.py
@@ -12,3 +12,13 @@ class Instance(object):
     def _init_(self):
         self.state = []
         self.dependencies = []
+
+    def launch(self)
+        # parameters
+            # none
+        # precondition
+            # not created, not running, not terminated (or the state array is empty);
+            # and whatever object the instance depends on
+            # has the relevant state before the instance is launched
+        # effect
+            # created and running
