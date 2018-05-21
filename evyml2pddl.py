30ba5d8679abb93bcf192bbf101faa1eda0ab8d8
Igor Tikhonin <ivtikhon@gmail.com>
2018-05-21 08:22:44 -0400

    Working on Evans YAML to PDDL converter

--- evyml2pddl.py
+++ evyml2pddl.py
@@ -2,3 +2,31 @@
 
 # Evans YAML to PDDL converter
 # Written by Igor Tikhonin in 2018
+
+import sys, getopt
+import yaml
+
+def parse_opt (argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print ('test.py -i <inputfile> -o <outputfile>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print ('test.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print ('Input file is "', inputfile)
+   print ('Output file is "', outputfile)
+
+with open("example.yaml", 'r') as stream:
+    try:
+        print(yaml.load(stream))
+    except yaml.YAMLError as exc:
+        print(exc)
