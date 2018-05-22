4376b1166f9b32b85944a76f168a7c9bc86c2974
Igor Tikhonin <ivtikhon@gmail.com>
2018-05-22 16:03:47 -0400

    Option parsing

--- evyml2pddl.py
+++ evyml2pddl.py
@@ -7,29 +7,35 @@ import sys, getopt
 import yaml
 
 def usage ():
-    print ('evyml2pddl.py [-o <outputfile>] input_file.yml')
+    print ('evyml2pddl.py [-h | --help] [-o <outputfile> | --output=<outputfile>] input_file.yml')
 
 def main (argv):
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:", ["help", "output="])
+        opts, args = getopt.getopt(argv, "ho:", ["help", "output="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
     output = None
+    input = None
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
             sys.exit()
         elif o in ("-o", "--output"):
             output = a
+    if len(args) == 1:
+        input = args[0]
+    else:
+        usage()
+        sys.exit()
+    print ('Input: ' + input)
     if output == None:
         print ('Output: stdout')
     else:
         print ('Output: ' + output)
-    print ('Input: ' + args)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
 
 # with open("example.yaml", 'r') as stream:
 #     try:
