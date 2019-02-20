c76bcaa82f37adf0a2cc74da5c6548a0b89bc296
Igor Tikhonin <ivtikhon@gmail.com>
2019-02-19 22:17:04 -0500

    Working on listener

--- evans.py
+++ evans.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
 import sys
+import pprint
 from antlr4 import *
 from EvansLexer import EvansLexer
 from EvansParser import EvansParser
+from EvansListener import EvansListener
+
+
+class PDDLConverter(EvansListener):
+    def exitClassDeclaration(self, ctx):
+        print('class: ' + ctx.ID().getText())
+
+    def exitMethodDeclaration(self, ctx):
+        print('method: ' + ctx.ID().getText())
+
+    def exitPredicateDeclaration(self, ctx):
+        print('pred: ' + ctx.ID().getText())
 
 def main(argv):
     input = FileStream(argv[1])
@@ -11,6 +24,10 @@ def main(argv):
     stream = CommonTokenStream(lexer)
     parser = EvansParser(stream)
     tree = parser.codeFile()
+    pddl_code = PDDLConverter()
+    walker = ParseTreeWalker()
+    walker.walk(pddl_code, tree)
+
 
 if __name__ == '__main__':
     main(sys.argv)
