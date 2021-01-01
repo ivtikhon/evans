1cbfa9ec0650a8f58623a69568f2f838b76376e2
Igor Tikhonin <ivtikhon@gmail.com>
2021-01-01 18:10:28 -0500

    Working on 8 queens problem

--- usecases/8queens/8queens.py
+++ usecases/8queens/8queens.py
@@ -1,118 +1,152 @@
 import subprocess
 import pprint
+import string
 
-class Plan:
-    def __init__(self, domain_file, problem_file):
-        self.actions = {}
-        self.objects = {}
-        self.planner = {
-            'path': '/vagrant/downward/fast-downward.py',
-            'options': '--evaluator "hff=ff()" --search "lazy_greedy([hff], preferred=[hff])"',
-            'result': 'sas_plan'
-        }
-        self.debug_opt = ['plan']
-        self.plan = None
-        self.domain_file = domain_file
-        self.problem_file = problem_file
+# class Plan:
+#     def __init__(self, domain_file, problem_file):
+#         self.actions = {}
+#         self.objects = {}
+#         self.planner = {
+#             'path': '/vagrant/downward/fast-downward.py',
+#             'options': '--evaluator "hff=ff()" --search "lazy_greedy([hff], preferred=[hff])"',
+#             'result': 'sas_plan'
+#         }
+#         self.debug_opt = ['plan']
+#         self.plan = None
+#         self.domain_file = domain_file
+#         self.problem_file = problem_file
     
-    def add_action(self, name, act):
-        self.actions[name] = act
+#     def add_action(self, name, act):
+#         self.actions[name] = act
 
-    def add_object(self, name, obj):
-        self.objects[name] = obj
+#     def add_object(self, name, obj):
+#         self.objects[name] = obj
 
-    def generate_plan(self):
-        plan = []
-        tempdir = '/tmp'
-        args = self.planner['path'] + ' ' + self.domain_file + ' ' + self.problem_file + ' ' + self.planner['options']
-        with subprocess.Popen(args, cwd=tempdir, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as planner:
-            planner.wait()
-            if 'planner_stdout' in self.debug_opt:
-                print('=== Planner output ===')
-                for line in planner.stdout:
-                    line = line.decode().rstrip()
-                    print(line)
-            if planner.returncode == 0: # planner generated a plan
-                if 'plan' in self.debug_opt:
-                    print('=== Plan ==')
-                    with open(tempdir + '/' + self.planner['result'], 'rt') as planfile:
-                        for line in planfile:
-                            if line.startswith(';'):
-                                continue
-                            l = line.rstrip()[1:-1]
-                            plan.append(l)
-                            if 'plan' in self.debug_opt:
-                                print(l)
-            else:
-                raise Exception("FAILURE: PDDL planner found no solution")
-        self.plan = plan
+#     def generate_plan(self):
+#         plan = []
+#         tempdir = '/tmp'
+#         args = self.planner['path'] + ' ' + self.domain_file + ' ' + self.problem_file + ' ' + self.planner['options']
+#         with subprocess.Popen(args, cwd=tempdir, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as planner:
+#             planner.wait()
+#             if 'planner_stdout' in self.debug_opt:
+#                 print('=== Planner output ===')
+#                 for line in planner.stdout:
+#                     line = line.decode().rstrip()
+#                     print(line)
+#             if planner.returncode == 0: # planner generated a plan
+#                 if 'plan' in self.debug_opt:
+#                     print('=== Plan ==')
+#                     with open(tempdir + '/' + self.planner['result'], 'rt') as planfile:
+#                         for line in planfile:
+#                             if line.startswith(';'):
+#                                 continue
+#                             l = line.rstrip()[1:-1]
+#                             plan.append(l)
+#                             if 'plan' in self.debug_opt:
+#                                 print(l)
+#             else:
+#                 raise Exception("FAILURE: Planner found no solution")
+#         self.plan = plan
 
 class Queen:
-    def __init__(self, index):
+    def __init__(self, number):
         self.placed = False
-        self.number = index
+        self.number = number
 
 class Cell:
-    def __init__(self, name):
+    def __init__(self, name, column, row):
         self.occupied = False
         self.name = name
-        self.__reacheable = []
+        self.column = column
+        self.row = row
+        self.maindiagonal = None
+        self.antidiagonal = None
+        self.reacheable = None
     
-    def reacheable(self, c):
-        return c.name in self.__reacheable
-
-class Chessboard:
-    def __init__(self):
-        # self.plan = Plan()
+class ChessBoard:
+    def __init__(self, dimension = 8):
         self.queens = []
-        self.cells = {}
+        self.cells = []
 
-        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-        numbers = range(8)
+        columns = []
+        rows = []
+        maindiagonals = []
+        antidiagonals = []
 
-        # Add queens
-        for index in range(1, 9):
-            q = Queen(index)
-            self.queens.append(q)
+        self.dimension = dimension
 
-        # Add cells
-        for letter in letters:
-            self.cells[letter] = [Cell(letter + str(num)) for num in numbers]
-        
-        # Specify neighbours for every cell (make the chess board)
-        for letter_index in range(len(letters)):
-            close_letters = [letter_index]
-            if letter_index > 0 and letter_index < len(letters) - 1:
-                close_letters.append(letter_index + 1)
-                close_letters.append(letter_index - 1)
-            elif letter_index == 0:
-                close_letters.append(letter_index + 1)
-            else:
-                close_letters.append(letter_index - 1)
-            for num in numbers:
-                close_num = [num]
-                if num > 0 and num < len(numbers) - 1:
-                    close_num.append(num + 1)
-                    close_num.append(num - 1)
-                elif num == 0:
-                    close_num.append(num + 1)
-                else:
-                    close_num.append(num - 1)
-                
-                print('Cell : ' + letters[letter_index] + str(num + 1))
-                for l in close_letters:
-                    for n in close_num:
-                        if l == letter_index and n == num:
-                            continue
-                        print(letters[l] + str(n + 1))
+        self.letters = list(string.ascii_lowercase[:dimension])
 
+        # Queens
+        for index in range(1, dimension + 1):
+            self.queens.append(Queen(index))
 
+        # Cells and columns
+        for i, letter in enumerate(self.letters):
+            column = []
+            for num in range(dimension):
+                c = Cell(letter + str(num + 1), i, num)
+                self.cells.append(c)
+                column.append(c)
+            columns.append(column)
 
-    def place_queen(self, q: Queen, c: Cell):
-        assert not q.placed and not c.occupied and not any(c1.occupied and c.reacheable(c1) for c1 in self.cells)
-        q.placed = True
-        c.occupied = True
+        # Rows
+        for i in range(dimension):
+            row = []
+            for j in range(dimension):
+                c = columns[j][i]
+                row.append(c)
+            rows.append(row)
+
+        # Diagonals
+        for p in range(dimension * 2 - 1):
+            mdiagonal = []
+            adiagonal = []
+            for q in range(max(0, p - dimension + 1), min(p, dimension - 1) + 1):
+                # Main diagonal
+                c = columns[q][p - q]
+                c.maindiagonal = p
+                mdiagonal.append(c)
+                # Antidiagonal
+                c = columns[dimension - 1 - q][p - q]
+                c.antidiagonal = p
+                adiagonal.append(c)
+            maindiagonals.append(mdiagonal)
+            antidiagonals.append(adiagonal)
+    
+        # Reacheability
+        for c in self.cells:
+            c.reacheable = list(set(columns[c.column] + rows[c.row] + maindiagonals[c.maindiagonal] + antidiagonals[c.antidiagonal]))
+
+    def print(self):
+        print(' ', end=' ')
+        for letter in self.letters:
+            print(letter, end='  ')
+        print('')
+        for i in range(self.dimension):
+            print(str(i + 1), end=' ')
+            for j in range(self.dimension):
+                c = self.cells[i + j * self.dimension]
+                content = self.letters[c.column] + str(c.row + 1)
+                # content = '.'
+                # if self.cells[i + j * self.dimension].occupied:
+                #     content = 'Q'
+                print(content, end=' ')
+            print('')
+
+def place_queen(q: Queen, c: Cell):
+    assert not q.placed
+    assert not c.occupied
+    assert not any([c1.occupied for c1 in c.reacheable])
+    q.placed = True
+    c.occupied = True
 
 if __name__ == "__main__":
-    board = Chessboard()
+    board = ChessBoard(8)
+    board.print()
+    for c in board.cells:
+        print(c.name, end=': ')
+        for c1 in sorted(c.reacheable, key=lambda x: x.name):
+            print(c1.name, end=' ')
+        print('')
     # board.plan.generate_plan()
