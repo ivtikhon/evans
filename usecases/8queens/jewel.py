90fff962b2b060e63533626a80b22013dfccfecc
Igor Tikhonin <ivtikhon@gmail.com>
2022-10-23 14:41:33 -0400

    Parsing action functions

--- usecases/8queens/jewel.py
+++ usecases/8queens/jewel.py
@@ -3,44 +3,54 @@ import beniget
 import gast
 import pprint
 
+class Action:
+    def __init__(self, function_node, chains):
+        self.name = function_node.name
+        print(function_node.name)
+
+        for local_var in chains.locals[function_node]:
+            print(local_var.name())
+            # print(dir(local_var))
+            # print(chains.chains[local_var.node])
+            for use in local_var.users():
+                print(use)
+
+class Class:
+    def __init__(self, name):
+        self.actions = {}
+        self.attributes = {}
+        
+        source_code = inspect.getsource(name)
+        module = gast.parse(source_code)
+        chains = beniget.DefUseChains()
+        chains.visit(module)
+
+        class_tree = module.body[0]
+        # Get the Actions class
+        action_class = [node for node in class_tree.body if isinstance(node, gast.ClassDef) and node.name == 'Actions']
+        if action_class:
+            for class_node in action_class[0].body:
+                if isinstance(class_node, gast.FunctionDef):
+                    # Action class contains functions decorated with the classmethod decorator
+                    if any(decorator.id == 'classmethod' for decorator in class_node.decorator_list):
+                        decorator_function = class_node
+                        # Decorator function is expected to return a function
+                        return_node = [function_node for function_node in decorator_function.body if isinstance(function_node, gast.Return) and isinstance(function_node.value, gast.Name)]
+                        action_function = None
+                        if return_node:
+                            action_function = [function_node for function_node in decorator_function.body if isinstance(function_node, gast.FunctionDef) and function_node.name == return_node[0].value.id]
+                        if action_function:
+                            self.actions[action_function[0].name] = Action(action_function[0], chains)
+
 class Plan:
     def __init__(self, objects, goal):
         self.objects = objects
+        self.classes = {}
         self.goal = goal
 
-    def generate_plan(self):
         for class_name in set([type(obj) for obj in self.objects]):
-            source_code = inspect.getsource(class_name)
-            module = gast.parse(source_code)
-            chains = beniget.DefUseChains()
-            chains.visit(module)
-            class_tree = module.body[0]
-            # Get the Actions class
-            actions_class = None
-            for node in class_tree.body:
-                if isinstance(node, gast.ClassDef) and node.name == 'Actions':
-                    actions_class = node
-                    break
-            # Parse actions functions
-            if actions_class:
-                for class_node in actions_class.body:
-                    if isinstance(class_node, gast.FunctionDef):
-                        # Action class contains functions decorated with the classmethod decorator
-                        if any(decorator.id == 'classmethod' for decorator in class_node.decorator_list):
-                            decorator_function = class_node
-                            # Decorator function is expected to return a function
-                            return_node = None
-                            action_function = None
-                            for function_node in decorator_function.body:
-                                if isinstance(function_node, gast.Return) and isinstance(function_node.value, gast.Name):
-                                    return_node = function_node
-                                    break
-                            if return_node:
-                                for function_node in decorator_function.body:
-                                    if isinstance(function_node, gast.FunctionDef) and function_node.name == return_node.value.id:
-                                        action_function = function_node
-                                        break
-                            if action_function:
-                                for local_var in chains.locals[action_function]:
-                                    print(local_var.name())
+            self.classes[class_name] = Class(class_name)
+
+    def generate_plan(self):
+        pass
 
