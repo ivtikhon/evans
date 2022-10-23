470662de414ea30356e792fa94d50bf431d000df
Igor Tikhonin <ivtikhon@gmail.com>
2022-10-23 19:37:01 -0400

    Parsing actions

--- usecases/8queens/jewel.py
+++ usecases/8queens/jewel.py
@@ -3,17 +3,31 @@ import beniget
 import gast
 import pprint
 
-class Action:
-    def __init__(self, function_node, chains):
-        self.name = function_node.name
-        print(function_node.name)
+class Action(gast.NodeVisitor):
+    def __init__(self, function_node, decorator_function, chains):
+        self.name = decorator_function
+        self.local_vars = {}
+        self.chains = chains
+        self.node = function_node
+        print(decorator_function)
 
-        for local_var in chains.locals[function_node]:
-            print(local_var.name())
-            # print(dir(local_var))
-            # print(chains.chains[local_var.node])
-            for use in local_var.users():
-                print(use)
+        for var in chains.locals[function_node]:
+            self.local_vars[var] = {'name': var.name}
+
+    def visit_Attribute(self, node):
+        print(f'{node.value.id} . {node.attr}')
+        for var in self.local_vars:
+            if node.value != var.node:
+                for use in var.users():
+                    if node.value == use.node:
+                        print(f'Use of {node.value.id}')
+        self.generic_visit(node)
+
+    
+    def visit_ListComp(self, node):
+        for var in self.chains.locals[node]:
+            self.local_vars[var] = {'name': var.name}
+        self.generic_visit(node)
 
 class Class:
     def __init__(self, name):
@@ -35,12 +49,20 @@ class Class:
                     if any(decorator.id == 'classmethod' for decorator in class_node.decorator_list):
                         decorator_function = class_node
                         # Decorator function is expected to return a function
-                        return_node = [function_node for function_node in decorator_function.body if isinstance(function_node, gast.Return) and isinstance(function_node.value, gast.Name)]
-                        action_function = None
+                        return_node = [
+                            function_node for function_node in decorator_function.body
+                                if isinstance(function_node, gast.Return) and isinstance(function_node.value, gast.Name)
+                        ]
                         if return_node:
-                            action_function = [function_node for function_node in decorator_function.body if isinstance(function_node, gast.FunctionDef) and function_node.name == return_node[0].value.id]
-                        if action_function:
-                            self.actions[action_function[0].name] = Action(action_function[0], chains)
+                            action_function = [
+                                function_node for function_node in decorator_function.body 
+                                    if isinstance(function_node, gast.FunctionDef) and function_node.name == return_node[0].value.id
+                            ]
+                            if action_function:
+                                function = action_function[0]
+                                action = Action(function, decorator_function.name, chains)
+                                self.actions[decorator_function.name] = action
+                                action.visit(function)
 
 class Plan:
     def __init__(self, objects, goal):
